a72e5055439dc99978d0afde0487425943f5e63a
AniruddhaChaudhury2006 <chaudhuryaniruddha56@gmail.com>
2026-04-26 01:05:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,65 +8,65 @@ from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn import metrics
 st.title("Big Mart Sales Prediction App")
-big_mart_data = pd.read_csv('Train_bigmart.csv')
-big_mart_data['Item_Weight'].fillna(big_mart_data['Item_Weight'].mean(), inplace=True)
-mode_of_outlet_size = big_mart_data.pivot_table(values = "Outlet_Size", columns = "Outlet_Type", aggfunc = (lambda x: x.mode()[0]))
-missing_values = big_mart_data['Outlet_Size'].isnull()
-big_mart_data.loc[missing_values, 'Outlet_Size'] = big_mart_data.loc[missing_values, 'Outlet_Type'].apply(lambda x: mode_of_outlet_size[x])
-big_mart_data.replace({"Item_Fat_Content": {'low fat' : 'Low Fat', 'LF' : 'Low Fat', 'reg' : 'Regular'}}, inplace=True)
-original_data = big_mart_data.copy()
-encoder_dict = {}
-for col in ["Item_Identifier", "Item_Fat_Content", "Item_Type", "Outlet_Identifier", "Outlet_Size", "Outlet_Location_Type", "Outlet_Type"]:
-  le = LabelEncoder()
-  big_mart_data[col] = le.fit_transform(big_mart_data[col])
-  encoder_dict[col] = le
-X = big_mart_data.drop(columns = "Item_Outlet_Sales", axis=1)
-Y = big_mart_data['Item_Outlet_Sales']
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=2)
-regressor=XGBRegressor()
-regressor.fit(X_train,Y_train)
-training_data_prediction=regressor.predict(X_train)
-st.write("Model Performance")
-r2_train=metrics.r2_score(Y_train,training_data_prediction)
-st.write("R squared value on training data = ",r2_train)
-test_data_prediction=regressor.predict(X_test)
-r2_test=metrics.r2_score(Y_test,test_data_prediction)
-st.write("R squared value on test data = ",r2_test)
+@st.cache_data
+def load_data():
+    df = pd.read_csv('Train_bigmart.csv')
+    df['Item_Weight'].fillna(df['Item_Weight'].mean(), inplace=True)
+    mode = df.pivot_table(values="Outlet_Size",columns="Outlet_Type",aggfunc=lambda x: x.mode()[0])
+    missing = df['Outlet_Size'].isnull()
+    df.loc[missing, 'Outlet_Size'] = df.loc[missing, 'Outlet_Type'].apply(lambda x: mode[x][0])
+    df['Outlet_Size'] = df['Outlet_Size'].astype(str)
+    df.replace({"Item_Fat_Content": {'low fat': 'Low Fat','LF': 'Low Fa'reg': 'Regular'}}, inplace=True)
+    return df
+data = load_data()
+original_data = data.copy()
+@st.cache_resource
+def encode_data(df):
+    encoders = {}
+    for col in ["Item_Identifier", "Item_Fat_Content", "Item_Type","Outlet_Identifier", "Outlet_Size","Outlet_Location_Type", "Outlet_Type"]:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+        encoders[col] = le
+    return df, encoders
+data, encoder_dict = encode_data(data)
+@st.cache_resource
+def train_model(df):
+    X = df.drop("Item_Outlet_Sales", axis=1)
+    Y = df["Item_Outlet_Sales"]
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+    model = XGBRegressor()
+    model.fit(X_train, Y_train)
+    return model, X, X_train, X_test, Y_train, Y_test
+model, X, X_train, X_test, Y_train, Y_test = train_model(data)
+st.subheader("Model Performance")
+st.write("Train R2:", metrics.r2_score(Y_train, model.predict(X_train)))
+st.write("Test R2:", metrics.r2_score(Y_test, model.predict(X_test)))
 st.subheader("Data Visualization")
-if st.checkbox("Show Distributions"):
+if st.checkbox("Show Item MRP Distribution"):
     fig, ax = plt.subplots()
-    sns.histplot(original_data['Item_MRP'], kd=True, ax=ax)
+    sns.histplot(original_data['Item_MRP'], kde=True, ax=ax)
     st.pyplot(fig)
 if st.checkbox("Show Outlet Type Count"):
-    fig, ax = plt.subplots(figsize=(8,4))
+    fig, ax = plt.subplots()
     sns.countplot(x='Outlet_Type', data=original_data, ax=ax)
-    plt.xticks(rotation=45)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
     st.pyplot(fig)
 st.subheader("Enter Product Details")
 def user_input():
-    Item_Identifier = st.selectbox("Item Identifier",original_data['Item_Identifier'].unique())
-    Item_Weight = st.number_input("Item Weight", value=9.3)
-    Item_Fat_Content = st.selectbox("Item Fat Content",original_data['Item_Fat_Content'].unique())
-    Item_Visibility = st.number_input("Item Visibility", value=0.016)
-    Item_Type = st.selectbox("Item Type",original_data['Item_Type'].unique())
-    Item_MRP = st.number_input("Item MRP", value=249.8)
-    Outlet_Identifier = st.selectbox("Outlet Identifier",original_data['Outlet_Identifier'].unique())
-    Outlet_Establishment_Year = st.number_input("Outlet Establishment Year", value=1999)
-    Outlet_Size = st.selectbox("Outlet Size", original_data['Outlet_Size'].unique())
-    Outlet_Location_Type = st.selectbox("Outlet Location Type", original_data['Outlet_Location_Type'].unique())
-    Outlet_Type = st.selectbox("Outlet Type",original_data['Outlet_Type'].unique())
-    return {"Item_Identifier": Item_Identifier,"Item_Weight": Item_Weight,"Item_Fat_Content": Item_Fat_Content,"Item_Visibility": Item_Visibility,"Item_Type": Item_Type,"Item_MRP": Item_MRP,"Outlet_Identifier": Outlet_Identifier,"Outlet_Establishment_Year": Outlet_Establishment_Year,"Outlet_Size": Outlet_Size,"Outlet_Location_Type": Outlet_Location_Type,"Outlet_Type": Outlet_Type}
+    return { "Item_Identifier": st.selectbox("Item Identifier", original_data['Item_Identifier'].unique()), "Item_Weight": st.number_input("Item Weight", min_value=0.0), "Item_Fat_Content": st.selectbox("Item Fat Content", original_data['Item_Fat_Content'].unique()), "Item_Visibility": st.number_input("Item Visibility", min_value=0.0), "Item_Type": st.selectbox("Item Type", original_data['Item_Type'].unique()), "Item_MRP": st.number_input("Item MRP", min_value=0.0),  "Outlet_Identifier": st.selectbox("Outlet Identifier", original_data['Outlet_Identifier'].unique()), "Outlet_Establishment_Year": st.number_input("Outlet Establishment Year", min_value=1980, max_value=2025), "Outlet_Size": st.selectbox("Outlet Size", original_data['Outlet_Size'].unique()), "Outlet_Location_Type": st.selectbox("Outlet Location Type", original_data['Outlet_Location_Type'].unique()), "Outlet_Type": st.selectbox("Outlet Type", original_data['Outlet_Type'].unique())}
 input_dict = user_input()
+error = False
 for col in encoder_dict:
-    input_dict[col] = encoder_dict[col].transform([input_dict[col]])[0]
-input_data = np.array(list(input_dict.values())).reshape(1, -1)
-if st.button("Predict Sales"):
-  prediction = regressor.predict(input_data)
-  st.success(f"Predicted Sales: ₹ {prediction[0]:.2f}")
-
-  
-
-
+    try:
+        input_dict[col] = encoder_dict[col].transform([input_dict[col]])[0]
+    except:
+        st.error(f"Invalid value for {col}")
+        error = True
+if not error:
+    input_data = np.array([input_dict[col] for col in X.columns]).reshape(1, -1)
+    if st.button("Predict Sales"):
+        pred = model.predict(input_data)
+        st.success(f"Predicted Sales: ₹ {pred[0]:.2f}")
 
 
 
